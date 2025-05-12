0a0b53c95a9d368f92f7424e807aab03af9b5fd8
Afonso <ac0009055@gmail.com>
2025-05-12 13:38:22 +0100

    last repo

--- trabalho_final/rastreador_habitos/app.py
+++ trabalho_final/rastreador_habitos/app.py
@@ -1,28 +1,30 @@
 from main import adicionar_utilizador,remover_utilizador,adicionar_habitos,remover_habitos,registar_progresso,listar_habitos,listar_utilizadores
 import utils
 
-
 def main():
+    """Função principal do programa"""
     op = 0
     while op != 8:
-        print("---Rastreador de Habitos---")
-        op = utils.menu(['Adicionar utilizadores','Remover utilizador','Adicionar habito','Remover habito','Listar habitos','Listar Utilizadores','Registar progresso','Sair'])
+        op = utils.menu(['Adicionar utilizador','Remover utilizador','Adicionar habitos','Remover habitos',
+                         'Listar habitos','Listar utilizadores','Registar progresso','Sair'],'Menu principal')
         if op == 8:
             break
-        if op == 1:
+        elif op == 1:
             adicionar_utilizador()
-        if op == 2:
+        elif op == 2:
             remover_utilizador()
-        if op == 3:
+        elif op == 3:
             adicionar_habitos()
-        if op == 4:
+        elif op == 4:
             remover_habitos()
-        if op == 5:
+        elif op == 5:
             listar_habitos()
-        if op == 6:
+        elif op == 6:
             listar_utilizadores()
-        if op == 7:
+        elif op == 7:
             registar_progresso()
-
-if __name__ == "__main__":
+        else:
+            print('Opção invalida. Tente novamente')
+        
+if __name__ == '__main__':
     main()
--- trabalho_final/rastreador_habitos/main.py
+++ trabalho_final/rastreador_habitos/main.py
@@ -3,56 +3,55 @@ from datetime import datetime,timedelta
 
 utilizadores = {}  #Criar um dicionario vazio
 
-# Função para adicionar os utilizadores
+
 def adicionar_utilizador():
+    """Função para adicionar utilizadores"""
     nome = utils.ler_string(3,'Nome do utilizador a adicionar:')
     if nome not in utilizadores:
+        # Adicionar o utilizador ao dicionario
         utilizadores[nome] = {
             'habitos': [],
             'dias totais': 0,
             'dias consecutivos': 0
         }
-        print(f'Utilizador "{nome}" adicionado.')
+        print(f'Utilizador "{nome}" adicionado com sucesso')
     else:
         print(f'Utilizador "{nome}" já existe.')
 
-#Função para remover utilizadores
 def remover_utilizador():
+    """Função para remover utilizadores"""
     nome = utils.ler_string(3,'Nome do utilizador a remover:')
     if nome in utilizadores:
+        # Remover o utilizador do dicionario
         del utilizadores[nome]
-        print(f'Utilizador {nome} removido com sucesso')
-        print(utilizadores)
+        print(f'Utilizador "{nome}" removido com sucesso')
     else:
-        print(f'Utilizador "{nome}" não existe.')
-
-# Função para adicionar habitos
-def adicionar_habitos():
-    nome = utils.ler_string(3, 'Digite o nome do utilizador:')
-    if nome in utilizadores:
-        habito = utils.ler_string(3, 'Insira o hábito a ser adicionado:')
-        if habito in utilizadores[nome]['habitos']:
-            print(f'Habito {habito} já existe para o utilizador {nome}')    
-        else:
-            #guardar os habitos num dicionario
-            novo = {'habitos':habito}
-            utilizadores[nome]['habitos'].append(novo)
-            print(f'Hábito "{habito}" adicionado para {nome}.')
-    else: 
         print(f'Utilizador "{nome}" não encontrado.')
-        op = input('Deseja adicionar o utilizador? (s/n):')
-        if op.lower() == 's':
-            adicionar_utilizador()
-            print(f'Utilizador {nome} adicionado')
 
-# Função para remover habitos
-def remover_habitos():
+def adicionar_habitos():
+    """Função para adicionar habitos a um utilizador"""
     nome = utils.ler_string(3,'Insira o nome do utilizador a pesquisar:')
     # verificar se o utilizador existe
     if nome in utilizadores:
-        # perguntar o habito a apagar
-        habito = utils.ler_string(3,'Habito a apagar:')
-        # se o habito existir apaga-lo
+        # perguntar o habito a adicionar
+        habito = utils.ler_string(3,'Habito a adicionar:')
+        # se o habito não existir adiciona-lo
+        if habito not in utilizadores[nome]['habitos']:
+            utilizadores[nome]['habitos'].append(habito)
+            print(f'Habito {habito} adicionado com sucesso')
+        else:
+            print(f'Habito {habito} já existe')
+    else:
+        print(f'Utilizador {nome} não encontrado')
+
+def remover_habitos():
+    """Função para remover habitos de um utilizador"""
+    nome = utils.ler_string(3,'Insira o nome do utilizador a pesquisar:')
+    # verificar se o utilizador existe
+    if nome in utilizadores:
+        # perguntar o habito a remover
+        habito = utils.ler_string(3,'Habito a remover:')
+        # se o habito existir remove-lo
         if habito in utilizadores[nome]['habitos']:
             utilizadores[nome]['habitos'].remove(habito)
             print(f'Habito {habito} removido com sucesso')
@@ -61,38 +60,57 @@ def remover_habitos():
     else:
         print(f'Utilizador {nome} não encontrado')
 
-# Função para listar os habitos, os dias consecutivos e o total de dias
 def listar_habitos():
-    nome = utils.ler_string(3,'Nome do utilizador a pesquisar:')
-    if nome in utilizadores:
-        for habito in utilizadores[nome]['habitos']:
-            print(f'Habitos: {utilizadores[nome]['habitos']}')
-            print(f'Total de dias: {utilizadores[nome]["dias totais"]}')
-            print(f'Dias consecutivos: {utilizadores[nome]["dias consecutivos"]}')
-    else:
-        print(f'Não existe um utilizador com o nome {nome}')
+    """Função para listar os habitos, os dias consecutivos e o total de dias"""
 
-# Função para listar os utilizadores
-def listar_utilizadores():
-    for utilizador in utilizadores:
-        print(utilizador)
-
-# Função para registar o progresso dos habitos
-def registar_progresso():
     nome = utils.ler_string(3,'Digite o nome do utilizador:')
-    habito = utils.ler_string(3,'Digite o hábito a ser registrado:')
-    #data atual
-    data_atual = datetime.now()
-    #str da data atual
-    str_data_atual = data_atual.strftime('%d-%m-%Y')
-    #data do dia anterior
-    data_ant = data_atual - timedelta(days = 1)
-    #str da data anterior
-    str_data_ant = data_ant.strftime('%d-%m-%Y')
-    if nome not in utilizadores:
-        print(f'Não foi possivel encontrar o utilizador {nome}.')
-        if habito not in utilizadores[nome]['habitos']:
-            print(f'Habito não existe para o utilizador {nome}')
+    if nome in utilizadores:
+        print(f'Habitos de {nome}:')
+        for habito in utilizadores[nome]['habitos']:
+            print(f'- {habito}')
+        print(f'Dias totais: {utilizadores[nome]["dias totais"]}')
+        print(f'Dias consecutivos: {utilizadores[nome]["dias consecutivos"]}')
     else:
-        utilizadores[nome]['habitos'].append(str_data_atual)
-        print(f'Hábito "{habito}" registado em {str_data_atual} para {nome}.')
+        print(f'Utilizador {nome} não encontrado')
+
+def listar_utilizadores():
+    """Função para listar os utilizadores"""
+    if utilizadores:
+        print('Utilizadores:')
+        for nome in utilizadores:
+            print(f'- {nome}')
+    else:
+        print('Não existem utilizadores registados')
+
+def registar_progresso():
+    """Função para registar o progresso dos habitos"""
+    nome = utils.ler_string(3,'Digite o nome do utilizador:')
+    if nome in utilizadores:
+        habito = utils.ler_string(3,'Digite o habito a registar:')
+        if habito in utilizadores[nome]['habitos']:
+            data = datetime.now()
+            # verificar se o habito foi registado no dia de hoje
+            if 'ultimohabito' not in utilizadores[nome]:
+                utilizadores[nome]['ultimohabito'] = {}
+            if habito not in utilizadores[nome]['ultimohabito']:
+                utilizadores[nome]['ultimohabito'][habito] = data
+                print(f'Habito {habito} registado com sucesso')
+                # aumentar os dias totais
+                utilizadores[nome]['dias totais'] += 1
+                # aumentar os dias consecutivos
+                if 'ultimo_registo' not in utilizadores[nome]:
+                    utilizadores[nome]['ultimo_registo'] = data
+                    utilizadores[nome]['dias consecutivos'] += 1
+                else:
+                    # verificar se o ultimo registo foi no dia anterior
+                    if (data - utilizadores[nome]['ultimo_registo']).days == 1:
+                        utilizadores[nome]['dias consecutivos'] += 1
+                    else:
+                        utilizadores[nome]['dias consecutivos'] = 1
+                    utilizadores[nome]['ultimo_registo'] = data
+            else:
+                print(f'Habito {habito} já foi registado hoje')
+        else:
+            print(f'Habito {habito} não encontrado')
+    else:
+        print(f'Utilizador {nome} não encontrado')
